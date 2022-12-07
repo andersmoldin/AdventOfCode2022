00188f31f018b37d0957f098bb3e01a34cda091a
andersmoldin <anders@moldin.com>
2022-12-07 10:34:18 +0100

    Day 6

--- src/AoC_2022/Day_06.cs
+++ src/AoC_2022/Day_06.cs
@@ -13,16 +13,28 @@ public class Day_06 : BaseDay
 
     public override ValueTask<string> Solve_1()
     {
-        var solution = _input;
-
-        return new("");
+        return FindMarker(_input.Single(), 4);
     }
 
     public override ValueTask<string> Solve_2()
     {
-        var solution = _input;
+        return FindMarker(_input.Single(), 14);
+    }
 
-        return new("");
+    private ValueTask<string> FindMarker(string input, int markerLength)
+    {
+        foreach (var (c, i) in input.Select((c, i) => (c, i)))
+        {
+            if (i > markerLength - 2)
+            {
+                if (input.Substring(i - (markerLength - 1), markerLength).Distinct().Count() == input.Substring(i - (markerLength - 1), markerLength).Length)
+                {
+                    return new((i + 1).ToString());
+                }
+            }
+        }
+
+        throw new Exception("No signal found.");
     }
 
     private List<string> ParseInput()
